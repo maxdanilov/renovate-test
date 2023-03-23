b2e2ba55ca6dc2304166842096c87e9955b941b2
Max Danilov <maxdanilov@users.noreply.github.com>
2023-03-23 11:06:59 +0100

    test

---
+++ app1/update.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+set -ex
+
+# renovate: datasource=github-tags depName=cert-manager/cert-manager
+VERSION=v1.10.1
+
+SCRIPT_PATH="$(dirname "$(realpath "$0")")"
+UPSTREAM_SOURCE="https://github.com/jetstack/cert-manager/releases/download/${VERSION}/cert-manager.yaml"
+UPSTREAM_TARGET="upstream/cert-manager.yml"
+
+cd "$SCRIPT_PATH"
+echo "Downloading ${UPSTREAM_SOURCE} to ${UPSTREAM_TARGET}"
+curl -L --fail "${UPSTREAM_SOURCE}" > "${UPSTREAM_TARGET}"
